a0fb4768386b12a35fa31fc9ead9ca3f2318894a
Daniel Rojas <danielrojas243@gmail.com>
2022-12-21 20:16:36 -0500

    mailchimp

--- footer.php
+++ footer.php
@@ -17,38 +17,29 @@
 				<h2>Únete a nuestro <strong>BOLETIN INFORMATIVO</strong></h2>
 				<p>¡Suscríbete a nuestra newsletter y tendrás acceso a promociones exclusivas y mucho más!</p>
 				<!-- Begin Mailchimp Signup Form -->
-					<div id="mc_embed_signup">
-						<form action="https://sannaofficial.us13.list-manage.com/subscribe/post?u=6c9b8c80439723dc706679f5b&amp;id=e7af778304&amp;f_id=00e996e2f0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_self">
-							<div id="mc_embed_signup_scroll">
-								<div class="indicates-required">
-									<span class="asterisk">*</span> indicates required
+				<div id="mc_embed_signup">
+					<form action="https://sannaofficial.us13.list-manage.com/subscribe/post?u=6c9b8c80439723dc706679f5b&amp;id=e7af778304&amp;f_id=00e996e2f0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_self">
+						<div id="mc_embed_signup_scroll">
+							<div class="indicates-required"></div>
+							<div class="mc-field-group">
+								<div class="icon">
+									<?php get_template_part('images/icons/email'); ?>
 								</div>
-								<div class="mc-field-group">
-									<label for="mce-EMAIL">Email Address  <span class="asterisk">*</span>
-									</label>
-										<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" required>
-										<span id="mce-EMAIL-HELPERTEXT" class="helper_text"></span>
-									</div>
-									<div id="mce-responses" class="clear foot">
-										<div class="response" id="mce-error-response" style="display:none"></div>
-										<div class="response" id="mce-success-response" style="display:none"></div>
-									</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-									<div style="position: absolute; left: -5000px;" aria-hidden="true">
-									<input type="text" name="b_6c9b8c80439723dc706679f5b_e7af778304" tabindex="-1" value="">
-								</div>
-									<div class="optionalParent">
-										<div class="clear foot">
-											<input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button">
-											<p class="brandingLogo">
-												<a href="http://eepurl.com/igMO_1" title="Mailchimp - email marketing made easy and fun">
-													<img src="https://eep.io/mc-cdn-images/template_images/branding_logo_text_dark_dtp.svg">
-												</a>
-											</p>
-										</div>
-									</div>
+								<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" placeholder="Correo Electronico" required>
+								<span id="mce-EMAIL-HELPERTEXT" class="helper_text"></span>
 							</div>
-						</form>
-					</div>
+							<div id="mce-responses">
+								<div class="response" id="mce-error-response" style="display:none"></div>
+								<div class="response" id="mce-success-response" style="display:none"></div>
+							</div>    
+							<!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+							<div style="position: absolute; left: -5000px;" aria-hidden="true">
+								<input type="text" name="b_6c9b8c80439723dc706679f5b_e7af778304" tabindex="-1" value="">
+							</div>
+							<input type="submit" value="Subscribe" name="ENVIAR" id="mc-embedded-subscribe" class="button">
+						</div>
+					</form>
+				</div>
 				<!--End mc_embed_signup-->
 			</div>
 		</div>
--- functions.php
+++ functions.php
@@ -29,3 +29,14 @@ add_theme_support( 'custom-logo', array(
 if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
+
+function change_multiple_texts( $translated ) {
+	$text       = array(
+			'RETURN TO SHOP' => 'Volver a la tienda',
+			'No products on your wishlist yet.' => 'No hay productos en su lista de deseos todavía.',
+
+	);
+	$translated = str_ireplace( array_keys( $text ), $text, $translated );
+	return $translated;
+}
+add_filter( 'gettext', 'change_multiple_texts', 20 );
