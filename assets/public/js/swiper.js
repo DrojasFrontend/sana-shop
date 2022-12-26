3789435384ccd8dfce0f1348befb7552ea4c18e7
Daniel Rojas <danielrojas243@gmail.com>
2022-12-26 11:54:49 -0500

    fix swiper scroll mobile

--- assets/public/js/swiper.js
+++ assets/public/js/swiper.js
@@ -7,18 +7,19 @@ var swiper2 = new Swiper(".swiper-full-image", {
   direction: 'vertical',
   slidesPerView: "auto",
   freeMode: true,
-  mousewheel: true,
-  allowTouchMove: false,
+  // mousewheel: true,
+  // allowTouchMove: false,
 
-  breakpoints: {
-    1024: {
-      allowTouchMove: true,
-    },
-  },
+  // breakpoints: {
+  //   1024: {
+  //     allowTouchMove: true,
+  //   },
+  // },
 
   scrollbar: {
     el: ".swiper-scrollbar",
   },
+  
   thumbs: {
     swiper: swiper,
   },
