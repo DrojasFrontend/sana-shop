da0c5a4d05636c4ba3d4fc1659100b6b0a0ecfc7
Daniel Rojas <danielrojas243@gmail.com>
2022-12-27 12:05:42 -0500

    fix header

--- template-parts/nav/logo.php
+++ template-parts/nav/logo.php
@@ -1,10 +1,14 @@
 <div class="header-logo">
   <?php if ( has_custom_logo() && is_front_page() ) { ?>
     <?php the_custom_logo();?>
-  <?php } elseif (!is_front_page() ) { ?>
-    <a href="/">
+    <a class="show" href="/">
       <img src="<?php the_field('logo_secondary', 'option'); ?>" alt="">
     </a>
+  <?php } elseif (!is_front_page() ) { ?>
+    <a class="show" href="/">
+      <img src="<?php the_field('logo_secondary', 'option'); ?>" alt="">
+    </a>
+    <?php the_custom_logo();?>
   <?php } else { ?>
     <h1 class="site-title">
       <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
