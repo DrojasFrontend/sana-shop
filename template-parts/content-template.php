28c9962917cdb254d378086f74837fc3a41024c6
Daniel Rojas <danielrojas243@gmail.com>
2023-01-04 09:19:26 -0500

    two columns and three column

--- template-parts/content-template.php
+++ template-parts/content-template.php
@@ -9,7 +9,7 @@
 
 ?>
 
-<div class="custom-template" data-scroll data-scroll-section>
+<div class="custom-template" data-scroll-section>
   <h1><?php the_title(); ?></h1>
   <?php the_content(); ?>
 </div>
--- woocommerce/content-product.php
+++ woocommerce/content-product.php
@@ -24,7 +24,7 @@ if ( empty( $product ) || ! $product->is_visible() ) {
 	return;
 }
 ?>
-<li <?php wc_product_class( 'custom-grid-product' ); ?>>
+<li <?php wc_product_class( 'custom-grid-product' ); ?> data-scroll>
 	<?php
 	/**
 	 * Hook: woocommerce_before_shop_loop_item.
